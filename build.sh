712cf2514dbe426ab7694cc49fd3226c594aa0eb
t-cool <hiroki802@hotmail.com>
2022-07-27 07:41:07 +0900

    updated

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ../
+
+claat export 1zZ6Z9jlsw3hPeqyuD4W-KJdobpiFV-33-5NoLNdgPm4
+
+cd clojure-data-structures-tutorial
+
+git add .
+
+git commit -m "updated"
+
+git push -u origin master
